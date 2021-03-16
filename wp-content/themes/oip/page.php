0ed44b2320bbb947f8a4275c1e60d216c2e03767
vbulatovic <vbulatovic@outlook.com>
2021-03-16 09:24:01 +0100

    initial setup

--- wp-content/themes/oip/page.php
+++ wp-content/themes/oip/page.php
@@ -2,15 +2,11 @@
 
 <div data-barba="wrapper">
     <main data-barba="container" data-barba-namespace="about">
-        <div class="heading-container">
-            <h1 class="main-heading is-animated">This is<br><span class="green-heading-bg">the about</span><br>screen.<br></h1>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php the_content(); ?>
-            <?php
-            endwhile;
-            wp_reset_query();
-            ?>
-        </div>
+
+        <section>
+
+        </section>
+
     </main>
 </div>
 
