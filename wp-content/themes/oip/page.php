2fd835cbcf62fdecf8aaf03f4bf63c4f1a8c7346
vbulatovic <vbulatovic@outlook.com>
2021-03-17 12:28:14 +0100

    added about pages

--- wp-content/themes/oip/page.php
+++ wp-content/themes/oip/page.php
@@ -3,8 +3,50 @@
 <div data-barba="wrapper">
     <main data-barba="container" data-barba-namespace="about">
 
-        <section>
+        <section class="about-hero custom-padding">
+            <div class="container-fluid">
+                <div class="row">
+                    <h1 class="col-12">
+                        Our work is serious, we are not.
+                    </h1>
+                    <p class="col-12">
+                        Culture
+                    </p>
+                    <h3 class="col-9">
+                        We are OIP Robotics. We believe in a perpetual source of ideas and
+                        people bringing them to You. We stand for technology serving business
+                        and humanity, not the other way around.
+                    </h3>
+                    <p class="col-9">
+                        We work in collaborative, everyone-pitch-in mindset where employees have autonomy
+                        to help customers the way they see fit, rather than following strict guidelines and scripts.
+                    </p>
+                </div>
+            </div>
+        </section>
 
+
+        <section class="board-redirection-link">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="board-redirection-box">
+                            <a class="custom-link" href="/contact-us">Contact Us <img class="ms-5" width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow"></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="board-redirection-box">
+                            <div>
+                                <h3>Meet the team</h3>
+                                <p>One of our clients, a leading wholesaler on the E&S market, had difficulties keeping up with hundreds of daily rece.</p>
+                                <div class="d-block text-md-end pb-4 pb-md-0">
+                                    <a class="custom-link" href="#"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
     </main>
