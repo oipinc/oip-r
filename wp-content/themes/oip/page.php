d236584475b76ebe617222d18fa1a016e92e09b7
vbulatovic <vbulatovic@outlook.com>
2021-03-18 09:31:44 +0100

    updated

--- wp-content/themes/oip/page.php
+++ wp-content/themes/oip/page.php
@@ -25,22 +25,102 @@
             </div>
         </section>
 
+        <section class="custom-padding">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <h6>Wizards behind the scenes</h6>
+                        <p>
+                            We’re artists, scientists, parents, trail runners, chefs, dancers, gamers, chess players, writers, big thinkers, storytellers,
+                            dog lovers, bikers, singers and much more. Above all else, we’re technology advocates: launching you to wherever you need to go.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/oip_team.png">
+                </div>
+            </div>
+        </section>
+
+        <section class="custom-padding">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>
+                            OIP Robotics - BE AN INSURTECH
+                        </h2>
+                        <p>
+                            OIPR started as an InsurTech department within OIP as an alternative to classic tech shops.
+                            We believed we could create better software and faster processes when insurance and technology were treated as equals.
+                            <br><br>
+                            We’re not just millions of code lines, but people behind it.
+                            We're a bunch of tech geeks who strive to transform businesses by creating tools that are smart and convenient.
+                            You will find us at the intersection between Software Engineering, Automation and Customer Satisfaction.
+                            Crafting the insurance-native tech, that addresses real problems and gets the job done.
+                        </p>
+                    </div>
+                    <div class="col-12">
+                        <a class="custom-link" href="#">
+                            <span>Contact us</span>
+                            <img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow">
+                        </a>
+                        <a class="custom-link" href="#">
+                            Find out more
+                            <img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
 
         <section class="board-redirection-link">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="board-redirection-box">
-                            <a class="custom-link" href="/contact-us">Contact Us <img class="ms-5" width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow"></a>
+                            <div>
+                                <h3>Software Developer</h3>
+                                <p>
+                                    <span>Full time job</span>
+                                    <span>Remote</span>
+                                </p>
+                                <p>
+                                    <span>
+                                        Application due date
+                                    </span>
+                                    Open until filled
+                                </p>
+                                <div class="d-block text-end pb-4 pb-md-0">
+                                    <a class="custom-link" href="#">
+                                        <img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow">
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="board-redirection-box">
                             <div>
-                                <h3>Meet the team</h3>
-                                <p>One of our clients, a leading wholesaler on the E&S market, had difficulties keeping up with hundreds of daily rece.</p>
-                                <div class="d-block text-md-end pb-4 pb-md-0">
-                                    <a class="custom-link" href="#"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow"></a>
+                                <h3>DevOps</h3>
+                                <p>
+                                    <span>Full time job</span>
+                                    <span>Remote</span>
+                                </p>
+                                <p>
+                                    <span>
+                                        Application due date
+                                    </span>
+                                    Open until filled
+                                </p>
+                                <div class="d-block text-end pb-4 pb-md-0">
+                                    <a class="custom-link" href="#">
+                                        <img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.svg" alt="Arrow">
+                                    </a>
                                 </div>
                             </div>
                         </div>
