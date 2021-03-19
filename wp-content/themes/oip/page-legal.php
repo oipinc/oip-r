b4153fe18cbd4e2a0e937a203d6f4ffbefd74f84
markomjoip <markomj@oip.bz>
2021-03-19 13:08:00 +0100

    Legal pages

--- wp-content/themes/oip/page-legal.php
+++ wp-content/themes/oip/page-legal.php
@@ -8,23 +8,29 @@
 
 <div data-barba="wrapper">
     <main data-barba="container" data-barba-namespace="terms-and-condition">
-        <section class="hero">
-            <div class="hero-content">
+
+        <div class="get-in-touch d-none d-xl-block">
+            <a class="text-white" href="/contact-us">
+                Get in touch
+            </a>
+        </div>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+            <section class="legal-hero custom-wrapper hero-gif">
                 <div class="container-fluid">
-                    <h1>Hero</h1>
+                    <div class="row">
+                        <div class="col-xl-9">
+                            <h1> <?php the_title(); ?></h1>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section class="legal-wrapper">
-            <div class="container">
-                <?php while ( have_posts() ) : the_post(); ?>
+            </section>
+            <section class="legal-wrapper custom-wrapper block">
+                <div class="container-fluid">
                     <?php the_content(); ?>
-                <?php
-                endwhile;
-                wp_reset_query();
-                ?>
-            </div>
-        </section>
+                </div>
+            </section>
+        <?php endwhile; wp_reset_query(); ?>
     </main>
 </div>
 
