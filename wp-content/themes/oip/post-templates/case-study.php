ab61eeeba3efce7ef577f73e353aefe82eba581c
markomjoip <markomj@oip.bz>
2021-03-24 15:13:46 +0100

    New case study

--- wp-content/themes/oip/post-templates/case-study.php
+++ wp-content/themes/oip/post-templates/case-study.php
@@ -1,180 +1,135 @@
 <div data-barba="wrapper">
     <main data-barba="container" data-barba-namespace="case-study-view">
         <?php
-            $appImage          = get_field('app_image');
-            $ourSolutionBottom = get_field('our_solution_bottom');
             $result            = get_field('results');
             $recommendedPost   = get_field('recommended_case_study');
+            $ourSolution = get_field('our_solution');
+            $problemStatement = get_field('problem_statement');
+            $appImage = get_field('app_image');
+            $workflow = get_field('workflow');
         ?>
 
         <!-- Hero -->
         <section class="case-study-hero case-study-wrapper custom-wrapper hero-gif">
             <div class="container-fluid">
-                <div class="case-study-intro">
-                    <h6 data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">Case Study</h6>
-                    <h1 data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine"><?php echo the_title(); ?></h1>
-                    <h6 data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">Overview</h6>
+                <h6 data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">Case Study</h6>
+                <h1 data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine"><?php echo the_title(); ?></h1>
+            </div>
+        </section>
+        <!-- end -->
+
+        <!-- Hero -->
+        <?php while ( have_posts() ) : the_post(); ?>
+            <section class="case-study-intro custom-wrapper">
+                <div class="container-fluid">
                     <div class="row">
                         <div class="col-xl-10">
+                            <h6 data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">Overview</h6>
                             <div data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                <?php while ( have_posts() ) : the_post(); ?>
-                                    <?php the_content(); ?>
-                                <?php endwhile; wp_reset_query(); ?>
+                                <?php the_content(); ?>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        <?php endwhile; wp_reset_query(); ?>
         <!-- end -->
 
         <!-- Problem -->
-        <section class="case-study-wrapper case-study-problem custom-wrapper">
-            <div class="container-fluid">
-                <div class="row" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                    <div class="col-lg-8 case-study-text-nav">
-                        <h2>Problem statement</h2>
+        <?php if ($problemStatement): ?>
+            <section class="case-study-wrapper case-study-problem custom-wrapper">
+                <div class="container-fluid">
+                    <div class="row" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                        <div class="col-lg-8 case-study-text-nav">
+                            <h2><?php echo $problemStatement['label']; ?></h2>
+                        </div>
                     </div>
-                </div>
-
-                <?php $count = 0; ?>
-                <?php if( have_rows('problem_solution') ): ?>
-                    <?php while( have_rows('problem_solution') ): the_row(); $count++;
-                        $image = get_sub_field('image');
-                        $retinaImage = get_sub_field('retina_image');
-                        $content = get_sub_field('content');
-                        ?>
-                        <div class="row <?php echo $count === 1 ? "case-study-problem-divider" : "" ?>">
-                            <?php if ($count === 1) { ?>
-                                <div class="col-lg-6 case-study-text-nav">
-                                    <div class="d-flex align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                        <span class="fw-bold">Problem statement</span>
-                                        <span class="line"></span>
-                                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 case-study-text-nav">
+                            <div class="d-flex align-items-center justify-content-between" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                                <span class="fw-bold"><?php echo $problemStatement['label']; ?></span>
+                                <span class="line"></span>
+                            </div>
+                            <?php if ($problemStatement['image']): ?>
+                                <div class="problem-statement-img text-center">
+                                    <img class="img-fluid" src="<?php echo $problemStatement['image']; ?>" alt="Aries">
                                 </div>
-                            <?php } else { ?>
-                                <div class="col-lg-6 text-center">
-                                    <?php if ($image && $retinaImage) { ?>
-                                        <img class="img-fluid mb-3 mb-lg-0" src="<?php echo $image; ?>" srcset="<?php echo $retinaImage; ?> 2x" alt="Image" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                    <?php }?>
-                                </div>
-                            <?php } ?>
-                            <div class="col-lg-6">
-                                <div data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                    <?php echo $content; ?>
-                                </div>
+                            <?php endif; ?>
+                        </div>
+                        <div class="col-lg-6">
+                            <div data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                                <?php echo $problemStatement['content']; ?>
                             </div>
                         </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </div>
-        </section>
+                    </div>
+                </div>
+            </section>
+        <?php endif; ?>
         <!-- end -->
 
         <!-- Our Solution -->
-        <section class="case-study-wrapper case-study-solution custom-wrapper">
-            <div class="container-fluid">
-                <div class="row" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                    <div class="col-lg-8 case-study-text-nav">
-                        <h2>Our Solution</h2>
+        <?php if($ourSolution): ?>
+            <section class="case-study-wrapper case-study-solution custom-wrapper">
+                <div class="container-fluid">
+                    <div class="row" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                        <div class="col-lg-8 case-study-text-nav">
+                            <h2><?php echo $ourSolution['label']; ?></h2>
+                        </div>
                     </div>
-                </div>
-                <?php
-                $ourSolutionTop = get_field('our_solution_top');
-                if($ourSolutionTop): ?>
                     <div class="row">
                         <div class="col-lg-6 case-study-text-nav">
-                            <div class="d-flex align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                <span class="fw-bold"><?php echo $ourSolutionTop['label']; ?></span>
+                            <div class="d-flex align-items-center justify-content-between" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                                <span class="fw-bold"><?php echo $ourSolution['label']; ?></span>
                                 <span class="line"></span>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                <?php echo $ourSolutionTop['content']; ?>
+                                <?php echo $ourSolution['content']; ?>
                             </div>
                         </div>
                     </div>
-                <?php endif; ?>
-            </div>
-        </section>
+                </div>
+            </section>
+        <?php endif; ?>
         <!-- end -->
 
-        <?php if($appImage['image'] && $appImage['retina_image']): ?>
-            <section class="app-img-wrapper">
+        <!-- App image -->
+        <?php if($appImage): ?>
+            <section class="app-img-wrapper text-center">
                 <img class="img-fluid" src="<?php echo $appImage['image']; ?>" srcset="<?php echo $appImage['retina_image']; ?> 2x" alt="App image" data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine">
             </section>
         <?php endif; ?>
+        <!-- end -->
 
-        <?php if($ourSolutionBottom['label'] && $ourSolutionBottom['content']): ?>
-            <section class="case-study-wrapper case-study-solution custom-wrapper d-none">
+        <!-- Workflow -->
+        <?php if($workflow): ?>
+            <section class="case-study-workflow">
                 <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-6 case-study-text-nav">
-                            <div class="d-flex align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                <span class="fw-bold"><?php echo $ourSolutionBottom['label']; ?></span>
-                                <span class="line"></span>
-                            </div>
+                    <?php echo $workflow['intro']; ?>
+
+                    <?php if($workflow['image']): ?>
+                        <div class="workflow-img text-center">
+                            <img class="img-fluid" src="<?php echo $workflow['image']; ?>" alt="Workflow">
                         </div>
-                        <div class="col-lg-6">
-                            <div data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                                <?php echo $ourSolutionBottom['content']; ?>
-                            </div>
-                        </div>
-                    </div>
+                    <?php endif; ?>
                 </div>
             </section>
         <?php endif; ?>
+        <!-- end -->
 
-        <?php if( have_rows('slider') ): ?>
-            <section class="case-study-slider-wrapper">
-                <div class="case-study-slider">
-                    <?php while( have_rows('slider') ): the_row();
-                        $image = get_sub_field('image');
-                        $retinaImage = get_sub_field('retina_image');
-                        $caption = get_sub_field('caption');
-                    ?>
-                        <div class="bg-transparent-blue case-study-slide-item" data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                            <img class="img-fluid" src="<?php echo $image; ?>" srcset="<?php echo $retinaImage; ?> 2x" alt="Slide">
-                            <div class="case-study-slide-caption">
-                                <?php echo $caption; ?>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                </div>
-            </section>
-        <?php endif; ?>
-
-        <?php if( have_rows('app_image_examples') ): ?>
-            <section>
-                <div class="container-fluid">
-                    <div class="row">
-                        <?php while( have_rows('app_image_examples') ): the_row();
-                            $image = get_sub_field('image');
-                            $retinaImage = get_sub_field('retina_image');
-                            $animationEffect = get_sub_field('animation_effect');
-                            $backgroundColor = get_sub_field('background_color');
-                            ?>
-                            <div class="col-md-6 use-case-img-holder text-center" style="background-color: <?php echo $backgroundColor; ?>">
-                                <img class="img-fluid" src="<?php echo $image; ?>" srcset="<?php echo $retinaImage; ?> 2x" alt="App image" data-aos="<?php echo $animationEffect; ?>" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                            </div>
-                        <?php endwhile; ?>
-                    </div>
-                </div>
-            </section>
-        <?php endif; ?>
-
-        <?php if($result['label'] && $result['content']): ?>
+        <!-- Result -->
+        <?php if($result): ?>
             <section class="case-study-wrapper case-study-result custom-wrapper">
                 <div class="container-fluid">
                     <div class="row" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
                         <div class="col-lg-8 case-study-text-nav">
-                            <h2>Results</h2>
+                            <h2><?php echo $result['label']; ?></h2>
                         </div>
                     </div>
                     <div class="row case-study-problem-divider">
                         <div class="col-lg-6 case-study-text-nav">
-                            <div class="d-flex align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                            <div class="d-flex align-items-center justify-content-between" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
                                 <span class="fw-bold"><?php echo $result['label']; ?></span>
                                 <span class="line"></span>
                             </div>
@@ -191,6 +146,7 @@
                 </div>
             </section>
         <?php endif; ?>
+        <!-- end -->
 
         <?php if($recommendedPost): ?>
             <section class="recommended-case-study bg-gray-secondary case-study-wrapper custom-wrapper">
