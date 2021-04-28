b28725d174fa959fa6c7b4d7258f535eb314edab
markomjoip <markomj@oip.bz>
2021-04-28 15:38:39 +0200

    Update

--- wp-content/themes/oip/assets/js/custom.js
+++ wp-content/themes/oip/assets/js/custom.js
@@ -246,20 +246,7 @@ jQuery(function() {
                 }
 
                 rect.on("click", function (e) {
-                    e.preventDefault();
-                    const title = jQuery(this).attr("data-title");
-                    const content = jQuery(this).attr("data-text");
-                    const image = jQuery(this).attr("data-profile");
-                    const el = jQuery(".profile-holder");
-
-                    el.find(".profile-title").text(title);
-                    el.find(".profile-content").html(content);
-                    el.find(".profile-img-holder").addClass("active");
-
-                    el.find(".profile-img").attr('src', image).load(function() {
-                        el.find(".profile-img-holder").removeClass("active");
-                    });
-                    el.addClass("active");
+                    alert('dsadas');
                 });
             },
             // Variations for didactical purpose…
